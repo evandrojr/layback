a688329f050d13fd9b0e44f8caf4e552752e47a1
Evandro Junior <evandrojr@gmail.com>
2016-01-12 18:46:38 -0300

    included remote control

--- bin/layback_loader.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-require 'rubygems'
-require 'bundler'
-
-Bundler.require(:default, :development, :test)
-
-system("crawl4subs ~/Downloads pob por spa eng bre ger")
-system("watch4subs ~/Downloads pob por spa eng bre ger")
-system("remote_control_server")
